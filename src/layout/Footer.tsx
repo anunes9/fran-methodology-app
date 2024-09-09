267baa9cf2f5af557feb587afc3280e00cdafbc1
Andre Nunes <andrenunes.work@gmail.com>
2024-09-09 01:25:50 +0100

    Fix build errors

--- src/layout/Footer.tsx
+++ src/layout/Footer.tsx
@@ -5,16 +5,26 @@ const TERMS_OF_SERVICE_URL = "https://www.franpadelproject.com/terms-of-service"
 
 export const Footer = () => (
   <footer className="text-foreground/50 text-xs flex justify-center items-center gap-4 mt-12">
-    <a href={PRIVACY_POLICY_URL} className="hover:underline" target="_blank">
+    <a
+      href={PRIVACY_POLICY_URL}
+      className="hover:underline"
+      target="_blank"
+      rel="noreferrer"
+    >
       Privacy Policy
     </a>
 
-    <a href={TERMS_OF_SERVICE_URL} className="hover:underline" target="_blank">
+    <a
+      href={TERMS_OF_SERVICE_URL}
+      className="hover:underline"
+      target="_blank"
+      rel="noreferrer"
+    >
       Terms of Service
     </a>
 
     <div className="flex items-center">
-      <a href="https://anunes9.github.io/me/" target="_blank">
+      <a href="https://anunes9.github.io/me/" target="_blank" rel="noreferrer">
         <img alt="an-logo" src={AN_LOGO} height={48} width={48} />
       </a>
 
